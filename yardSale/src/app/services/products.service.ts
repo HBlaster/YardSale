ebdeab7e8290a7ef1d338babc6b3d972130c149c
Alfredo Cano <alfredo0396.c@gmail.com>
2023-10-31 22:16:05 -0600

    se provoca error en api para probar observales y retry

--- yardSale/src/app/services/products.service.ts
+++ yardSale/src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpParams} from '@angular/common/http'
 import { product, CreateProductDTO, UpdateproductDTO } from '../models/product.model';
+import { retry } from 'rxjs/operators';
 
 
 
@@ -9,13 +10,22 @@ import { product, CreateProductDTO, UpdateproductDTO } from '../models/product.m
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = 'https://young-sands-07814.herokuappiiii.com/api/products';
   constructor(
     private http: HttpClient
   ) { }
 
-  getAllProducts(){
-    return this.http.get<product[]>(this.apiUrl);
+  getAllProducts(limit?: number, offset?:number ){
+    let params = new HttpParams();
+    if(limit && offset){
+      params = params.set('limit', limit);
+      params = params.set('offset', offset);
+      
+    }
+    return this.http.get<product[]>(this.apiUrl, {params})
+      .pipe(
+        retry(3)
+      );
   }
 
   getProductsByPage(limit: number, offset:number ){
