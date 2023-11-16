2e522a6c696104ab77245ee229fd27fd9120e9e1
Alfredo Cano <alfredo0396.c@gmail.com>
2023-11-15 23:29:46 -0600

    interceptores para login

--- yardSale/src/app/components/nav/nav.component.ts
+++ yardSale/src/app/components/nav/nav.component.ts
@@ -14,7 +14,7 @@ export class NavComponent implements OnInit {
 
   activeMenu = false;
   counter = 0;
-  token = '';
+  // token = '';
   profile:User | null = null;
 
   constructor(
@@ -33,37 +33,27 @@ export class NavComponent implements OnInit {
     this.activeMenu = !this.activeMenu;
   }
 
-  login() {
-    this.AuthService.login('sebas@mail.com', '1212')
-      .pipe(
-        switchMap((rta) => {
-          this.token = rta.access_token;
-          console.log(this.token);
-          return this.AuthService.profile(this.token);
-        })
-      )
-      .subscribe((user) => {
-        console.log(user);
-        this.profile = user;
-      });
+  // login() {
+  //   this.AuthService.login('sebas@mail.com', '1212')
+  //     .pipe(
+  //       switchMap((rta) => {
+  //         this.token = rta.access_token;
+  //         console.log(this.token);
+  //         return this.AuthService.getProfile(this.token);
+  //       })
+  //     )
+  //     .subscribe((user) => {
+  //       console.log(user);
+  //       this.profile = user;
+  //     });
+  // }
+
+  login(){
+    this.AuthService.loginAndGet('sebas@mail.com', '1212')
+    .subscribe(user =>{
+      this.profile= user;
+    });
   }
 
-  // login(){
-  //   this.AuthService.login(
-  //     'sebas@mail.com','1212'
-  //   ).subscribe(rta =>{
-  //     this.token = rta.access_token;
-  //     console.log(this.token);
-  //     this.getProfile();
-  //   });
-  // }
-
-  // getProfile(){
-  //   this.AuthService.profile(this.token)
-  //   .subscribe(user=>{
-  //     console.log(user);
-  //     this.profile = user;
-  //   });
-  // }
 
 }
--- yardSale/src/app/services/auth.service.ts
+++ yardSale/src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { switchMap, tap } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment';
 import { Auth } from '../models/auth.model';
 import { User } from '../models/user.model';
+import {TokenService} from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,23 +15,36 @@ export class AuthService {
   private apiUrl = `${environment.API_URL}/api/auth`;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private TokenService: TokenService
   ) { }
 
   login(email: string, password: string){
-    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password});
+    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password})
+    .pipe(
+      tap(response => this.TokenService.saveToken(response.access_token))
+    );
   }
-  profile(token: string){
-    const headers = new HttpHeaders();
-    headers.set('Authorization', `Bearer ${token}`);
+
+
+  getProfile(){
+    // const headers = new HttpHeaders();
+    // headers.set('Authorization', `Bearer ${token}`);
     return this.http.get<User>(`${this.apiUrl}/profile`, 
     // {headers}
     {
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
+      // headers:{
+      //   Authorization: `Bearer ${token}`
+      // }
       
     }
     );
+  }
+
+  loginAndGet(email: string, password: string){
+    return this.login(email, password)
+    .pipe(
+      switchMap(() => this.getProfile()),
+    )
   }
 }
