bc9cd533cc0e931595390c8c6abf5e68f47d4ad4
Alfredo Cano <alfredo0396.c@gmail.com>
2023-11-16 22:48:27 -0600

    carga y descarga de archivos

--- yardSale/src/app/app.component.ts
+++ yardSale/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import {AuthService} from './services/auth.service';
 import {UsersService} from './services/users.service';
 import { __core_private_testing_placeholder__ } from '@angular/core/testing';
+import {FilesService} from './services/files.service'
 
 
 @Component({
@@ -14,10 +15,12 @@ export class AppComponent {
   title = 'yardSale';
   imgParent= 'https://source.unsplash.com/random';
   token = '';
+  imgRta = '';
 
   constructor(
     private AuthService: AuthService,
-    private UsersService: UsersService
+    private UsersService: UsersService,
+    private FileServices: FilesService
   ){}
 
   createUser(){
@@ -29,4 +32,24 @@ export class AppComponent {
       console.log(rta);
     });
   }
+
+  downloadPdf(){
+    this.FileServices.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe();
+  }
+
+  onUpload(event: Event){
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if(file){
+      this.FileServices.uploadFile(file)
+    .subscribe(rta =>{
+
+      this.imgRta = rta.location;
+
+    })
+    }
+    
+  }
+
 }
